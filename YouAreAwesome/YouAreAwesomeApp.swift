7dee74f505d5a1d2bbae77813ea10f3fe8d8a44a
StellaByteStarlight <foam_pulses.6n@icloud.com>
2025-03-24 22:10:22 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Brendan on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
